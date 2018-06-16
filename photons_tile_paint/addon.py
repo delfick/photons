112fabfdf77376c3c749380b18b743ad993b42e1
Stephen Moore <stephen@delfick.com>
2018-06-16 13:15:27 +1000

    Being a little bit less repetitive

--- photons_tile_paint/addon.py
+++ photons_tile_paint/addon.py
@@ -14,62 +14,66 @@ __shortdesc__ = "Utilities for painting on the tiles"
 def __lifx__(*args, **kwargs):
     pass
 
-@an_action(needs_target=True, special_reference=True)
-async def tile_time(collector, target, reference, **kwargs):
+def animation_action(name, animationkls, optionskls, __doc__):
     """
-    Print time to the tiles
-
-    ``lifx lan:tile_time <reference>``
+    Return an action that will create our options and provide them to the animation kls
+    before running the animation
     """
-    from photons_tile_paint.time.animation import TileTimeAnimation
-    from photons_tile_paint.time.options import TileTimeOptions
+    async def action(collector, target, reference, **kwargs):
+        extra = collector.configuration["photons_app"].extra_as_json
+        options = optionskls.FieldSpec().normalise(Meta.empty(), extra)
+        async with ATarget(target) as afr:
+            await animationkls(target, afr, options).animate(reference)
 
-    extra = collector.configuration["photons_app"].extra_as_json
-    options = TileTimeOptions.FieldSpec().normalise(Meta.empty(), extra)
-    async with ATarget(target) as afr:
-        await TileTimeAnimation(target, afr, options).animate(reference)
+    action.__name__ = name
+    action.__doc__ = __doc__
 
-@an_action(needs_target=True, special_reference=True)
-async def tile_marquee(collector, target, reference, **kwargs):
-    """
-    Print scrolling text to the tiles
+    return action
 
-    ``lifx lan:tile_marquee <reference> -- '{"text": "hello there"}'``
-    """
-    from photons_tile_paint.marquee.animation import TileMarqueeAnimation
-    from photons_tile_paint.marquee.options import TileMarqueeOptions
+from photons_tile_paint.time.animation import TileTimeAnimation
+from photons_tile_paint.time.options import TileTimeOptions
+tile_time = an_action(needs_target=True, special_reference=True)(animation_action(
+      "tile_time"
+    , TileTimeAnimation, TileTimeOptions
+    , """
+      Print time to the tiles
 
-    extra = collector.configuration["photons_app"].extra_as_json
-    options = TileMarqueeOptions.FieldSpec().normalise(Meta.empty(), extra)
-    async with ATarget(target) as afr:
-        await TileMarqueeAnimation(target, afr, options).animate(reference)
+      ``lifx lan:tile_time <reference>``
+      """
+    ))
 
-@an_action(needs_target=True, special_reference=True)
-async def tile_pacman(collector, target, reference, **kwargs):
-    """
-    Make pacman go back and forth across your tiles
+from photons_tile_paint.marquee.animation import TileMarqueeAnimation
+from photons_tile_paint.marquee.options import TileMarqueeOptions
+tile_marquee = an_action(needs_target=True, special_reference=True)(animation_action(
+      "tile_marquee"
+    , TileMarqueeAnimation, TileMarqueeOptions
+    , """
+      Print scrolling text to the tiles
 
-    ``lifx lan:tile_pacman <reference>``
-    """
-    from photons_tile_paint.pacman.animation import TilePacmanAnimation
-    from photons_tile_paint.pacman.options import TilePacmanOptions
+      ``lifx lan:tile_marquee <reference> -- '{"text": "hello there"}'``
+      """
+    ))
 
-    extra = collector.configuration["photons_app"].extra_as_json
-    options = TilePacmanOptions.FieldSpec().normalise(Meta.empty(), extra)
-    async with ATarget(target) as afr:
-        await TilePacmanAnimation(target, afr, options).animate(reference)
+from photons_tile_paint.pacman.animation import TilePacmanAnimation
+from photons_tile_paint.pacman.options import TilePacmanOptions
+tile_pacman = an_action(needs_target=True, special_reference=True)(animation_action(
+      "tile_pacman"
+    , TilePacmanAnimation, TilePacmanOptions
+    , """
+      Make pacman go back and forth across your tiles
 
-@an_action(needs_target=True, special_reference=True)
-async def tile_nyan(collector, target, reference, **kwargs):
-    """
-    Make nyan go back and forth across your tiles
+      ``lifx lan:tile_pacman <reference>``
+      """
+    ))
 
-    ``lifx lan:tile_nyan <reference>``
-    """
-    from photons_tile_paint.nyan import NyanCatAnimation
-    from photons_tile_paint.nyan import NyanCatOptions
+from photons_tile_paint.nyan import TileNyanAnimation
+from photons_tile_paint.nyan import TileNyanOptions
+tile_nyan = an_action(needs_target=True, special_reference=True)(animation_action(
+      "tile_nyan"
+    , TileNyanAnimation, TileNyanOptions
+    , """
+      Make nyan go back and forth across your tiles
 
-    extra = collector.configuration["photons_app"].extra_as_json
-    options = NyanCatOptions.FieldSpec().normalise(Meta.empty(), extra)
-    async with ATarget(target) as afr:
-        await NyanCatAnimation(target, afr, options).animate(reference)
+      ``lifx lan:tile_nyan <reference>``
+      """
+    ))
