c308c2c7fb8044708ea436e14f15cce241be140a
Stephen Moore <stephen@delfick.com>
2020-01-04 18:03:30 +1100

    Make sure we don't return the same retry options every time

--- photons_transport/session/memory.py
+++ photons_transport/session/memory.py
@@ -16,13 +16,10 @@ class MemoryRetryOptions(RetryOptions):
     finish_multi_gap = 0.1
 
 
-memory_retry_options = MemoryRetryOptions()
-
-
 def makeMemorySession(basedon):
     class MemorySession(basedon):
         def retry_options_for(self, packet, transport):
-            return memory_retry_options
+            return MemoryRetryOptions()
 
         async def determine_needed_transport(self, packet, services):
             if MemoryService in services:
