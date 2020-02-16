7868920d2dee8ed1d26c17895efe96a977a67ee9
Stephen Moore <stephen@delfick.com>
2020-02-16 14:14:28 +1100

    [pytest] Remove unittest.TestCase related code

--- photons_app/test_helpers.py
+++ photons_app/test_helpers.py
@@ -1,13 +1,8 @@
 from photons_app.errors import PhotonsAppError
 
-from delfick_project.errors import DelfickErrorTestMixin
-from asynctest import TestCase as AsyncTestCase
 from contextlib import contextmanager
 from delfick_project.norms import sb
 from collections import defaultdict
-from unittest import TestCase
-from unittest import mock
-import asyncio
 import os
 
 
@@ -70,133 +65,6 @@ def assert_payloads_equals(payload, expected):
         print(f"KEY: {k} |:| GOT: {got} |:| WANT: {want}")
 
     assert len(different) == 0
-
-
-class FakeTarget(object):
-    def __init__(self, afr_maker=None):
-        self.call = -1
-        self.afr_maker = afr_maker
-        self.expected_run_with = []
-
-    async def args_for_run(self):
-        return self.afr_maker()
-
-    async def close_args_for_run(self, afr):
-        afr.close()
-
-    def session(self):
-        info = {}
-
-        class Session:
-            async def __aenter__(s):
-                afr = info["afr"] = await self.args_for_run()
-                return afr
-
-            async def __aexit__(s, exc_type, exc, tb):
-                if "afr" in info:
-                    await self.close_args_for_run(info["afr"])
-
-        return Session()
-
-    def script(self, part):
-        return FakeScript(self, part)
-
-    async def run_with(self, *args, **kwargs):
-        self.call += 1
-        call = mock.call(*args, **kwargs)
-
-        def add_error(e, do_raise=True):
-            if "error_catcher" in kwargs:
-                if type(kwargs["error_catcher"]) is list:
-                    kwargs["error_catcher"].append(e)
-                else:
-                    kwargs["error_catcher"](e)
-                return True
-            else:
-                if do_raise:
-                    raise e
-
-        if len(self.expected_run_with) < self.call:
-            add_error(BadTest("Got an extra call to the target", got=repr(call)))
-
-        if self.expected_run_with[self.call][0] != call:
-            try:
-                TestCase().assertEqual(call, self.expected_run_with[self.call][0])
-            except AssertionError as error:
-                print("---- EXPECTED DIFFERENT CALL for call {0} ----".format(self.call))
-                wanted = self.expected_run_with[self.call][0]
-
-                cargs = call[1]
-                wargs = wanted[1]
-                for i, (c, w) in enumerate(zip(cargs, wargs)):
-                    if c != w:
-                        print("\tDIFFERENT: item {0}".format(i))
-
-                        if hasattr(c, "pack") and hasattr(w, "pack"):
-                            print_packet_difference(c, w)
-                        else:
-                            print("\tGOT : {0}".format(c))
-                            print("\tWANT: {0}".format(w))
-
-                ckwargs = call[2]
-                wkwargs = wanted[2]
-                if ckwargs != wkwargs:
-                    print("\tKWARGS DIFFERENT")
-                    print("\tGOT : {0}".format(ckwargs))
-                    print("\tWANT: {0}".format(wkwargs))
-
-                if not add_error(error):
-                    raise
-
-        ret = self.expected_run_with[self.call][1]
-        if isinstance(ret, Exception):
-            add_error(ret)
-            raise ret
-        else:
-            for thing in ret:
-                yield thing
-
-    def expect_call(self, call, result):
-        self.expected_run_with.append((call, result))
-
-
-class TestCase(TestCase, DelfickErrorTestMixin):
-    pass
-
-
-class AsyncTestCase(AsyncTestCase, DelfickErrorTestMixin):
-    async def wait_for(self, fut, timeout=1):
-        try:
-            return await asyncio.wait_for(fut, timeout=timeout)
-        except asyncio.TimeoutError as error:
-            assert False, "Failed to wait for future before timeout: {0}".format(error)
-
-
-def with_timeout(func):
-    """
-    A decorator that returns an async function that runs the original function
-    with a timeout
-
-    It assumes you are decorating a method on a class with a ``wait_for`` method
-
-    .. code-block:: python
-
-        from photons_app.test_helpers import AsyncTestCase, with_timeout
-
-        import asyncio
-
-        class TestSomething(AsyncTestCase):
-            @with_timeout
-            async def test_waiting(self):
-                # This will assert False cause the function takes too long
-                await asyncio.sleep(20)
-    """
-
-    async def test(s):
-        await s.wait_for(func(s))
-
-    test.__name__ = func.__name__
-    return test
 
 
 def assertFutCallbacks(fut, *cbs):
