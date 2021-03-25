5c5c933cd3d1a99b8cc5d56fe1e3ba9486430fc0
Stephen Moore <stephen@delfick.com>
2021-03-25 21:49:13 +1100

    [app/actions] add class to running_main test

--- modules/tests/photons_app_tests/running_main/test_running_main.py
+++ modules/tests/photons_app_tests/running_main/test_running_main.py
@@ -28,3 +28,24 @@ describe "Running a mainline":
             process.stdout.decode().strip()
             == "8AC5B50C-7278-46F8-A164-9A75E310A466.39786789-9B56-475C-8F26-D04CE48EB206"
         )
+
+    it "works with old an_action":
+        process = subprocess.run(
+            [
+                sys.executable,
+                os.path.join(example_dir, "runner.py"),
+                "do_the_thing_with_old_an_action",
+                "--config",
+                os.path.join(example_dir, "config.yml"),
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        assert process.returncode == 0, "STDERR: {}\nSTDOUT:{}".format(
+            process.stderr, process.stdout
+        )
+        assert (
+            process.stdout.decode().strip()
+            == "8AC5B50C-7278-46F8-A164-9A75E310A466.39786789-9B56-475C-8F26-D04CE48EB206"
+        )
