3f93017a23f16f172238fd1c8edb5020090ba617
Stephen Moore <stephen@delfick.com>
2022-12-28 11:08:56 +1100

    web_server: moving test helpers around

---
+++ modules/photons_web_server/pytest_helpers.py
@@ -0,0 +1,163 @@
+import asyncio
+import json
+import types
+import typing as tp
+
+import aiohttp
+import pytest
+import websockets
+import websockets.client
+from photons_app import helpers as hp
+from photons_web_server.server import Server
+
+
+class IdentifierMatch:
+    identifier: str | None
+
+    def __init__(self, identifiers: set, *, count=2):
+        self.identifier = None
+        self.identifiers = identifiers
+        self.count = count
+
+    def __eq__(self, other: object) -> bool:
+        if self.identifier is None:
+            assert other not in self.identifiers
+            assert isinstance(other, str)
+            self.identifiers.add(other)
+            self.identifier = other
+            return True
+
+        return self.identifier == other
+
+    def __repr__(self) -> str:
+        if self.identifier is None:
+            return "<IDENTIFIER>"
+        else:
+            return repr(self.identifier)
+
+
+class Stream(hp.AsyncCMMixin):
+    _websocket: websockets.client.WebSocketClientProtocol
+
+    def __init__(self, address: str):
+        self.address = address
+
+    async def start(self) -> "Stream":
+        self._websocket = await websockets.client.connect(self.address)
+        return self
+
+    async def finish(
+        self,
+        exc_type: type[BaseException] | None = None,
+        exc: BaseException | None = None,
+        tb: types.TracebackType | None = None,
+    ):
+        if hasattr(self, "_websocket"):
+            await self._websocket.close()
+            del self._websocket
+
+    async def send(self, item: str | dict | list) -> None:
+        if not hasattr(self, "_websocket"):
+            raise AssertionError("Need to use Stream in a context manager")
+
+        if not isinstance(item, str):
+            await self._websocket.send(json.dumps(item))
+        else:
+            await self._websocket.send(item)
+
+    async def recv(self) -> dict | None:
+        try:
+            got: str | bytes = await self._websocket.recv()
+        except websockets.exceptions.ConnectionClosedOK:
+            return None
+
+        if got is None:
+            raise AssertionError("Got None back from the stream, except a dictionary")
+
+        if isinstance(got, bytes):
+            got = got.decode()
+
+        res = json.loads(got)
+        if not isinstance(res, dict):
+            raise AssertionError(f"Expected a dictionary from stream, got {res}")
+
+        return res
+
+
+class WebServerRoutes(hp.AsyncCMMixin):
+    def __init__(
+        self,
+        final_future: asyncio.Future,
+        setup_routes: tp.Callable[[Server], tp.Coroutine[None, None, None]] | None = None,
+        server_properties: dict | None = None,
+    ):
+        self.final_future = final_future
+        self.setup_routes = setup_routes
+        self.server_properties = server_properties
+        if self.server_properties is None:
+            self.server_properties = {}
+
+        self.task_holder = hp.TaskHolder(
+            self.final_future, name="WebServerRoutes::__init__[task_holder]"
+        )
+        self.graceful_final_future = hp.create_future(
+            name="WebServerRoutes::__init__[graceful_final_future]"
+        )
+
+    async def start(self, kls: type[Server] = Server) -> "WebServerRoutes":
+        assert self.server_properties is not None
+        WithProperties = type("S", (kls,), self.server_properties)  # type: tp.Any
+
+        class S(WithProperties):
+            async def setup_routes(s):
+                await super().setup_routes()
+                await self.setup_routes(s)
+
+        await self.task_holder.start()
+
+        self.server = S(
+            self.task_holder,
+            self.final_future,
+            self.graceful_final_future,
+        )
+
+        self.port = pytest.helpers.free_port()
+        self.serve_task = hp.get_event_loop().create_task(self.server.serve("127.0.0.1", self.port))
+        try:
+            await pytest.helpers.wait_for_port(self.port)
+        except AssertionError:
+            await self.serve_task
+            raise
+        return self
+
+    def stop(self) -> None:
+        self.graceful_final_future.cancel()
+
+    def start_request(
+        self, method: str, route: str, body: dict | None = None
+    ) -> asyncio.Task[aiohttp.ClientResponse]:
+        async def request() -> aiohttp.ClientResponse:
+            async with aiohttp.ClientSession() as session:
+                return await getattr(session, method.lower())(
+                    f"http://127.0.0.1:{self.port}{route}", json=body
+                )
+
+        return hp.get_event_loop().create_task(request())
+
+    def stream(self, route: str, *items) -> Stream:
+        return Stream(f"ws://127.0.0.1:{self.port}{route}")
+
+    async def finish(
+        self,
+        exc_typ: type[BaseException] | None = None,
+        exc: BaseException | None = None,
+        tb: types.TracebackType | None = None,
+    ) -> None:
+        self.graceful_final_future.cancel()
+        hp.get_event_loop().call_later(20, self.serve_task.cancel)
+        await hp.wait_for_all_futures(self.serve_task)
+        try:
+            await self.server.finished()
+        except:
+            self.final_future.cancel()
+            await self.task_holder.finish()
--- modules/tests/photons_web_server_tests/conftest.py
+++ modules/tests/photons_web_server_tests/conftest.py
@@ -1,17 +1,12 @@
 import asyncio
-import json
 import typing as tp
 
-import aiohttp
 import pytest
-import websockets
 from photons_app import helpers as hp
-from photons_web_server.server import Server
 from sanic import Sanic
 
 
 def pytest_configure(config):
-    pytest.helpers.register(WebServerRoutes)
     pytest.helpers.register(IsInstance)
 
 
@@ -65,91 +60,3 @@ class IsInstance:
             return f"<INSTANCE OF {self.kls}>"
         else:
             return repr(self.got)
-
-
-class WebServerRoutes(hp.AsyncCMMixin):
-    def __init__(
-        self,
-        final_future: asyncio.Future,
-        setup_routes: tp.Callable[["WebServerRoutes"], None] | None = None,
-        server_properties: dict | None = None,
-    ):
-        self.final_future = final_future
-        self.setup_routes = setup_routes
-        self.server_properties = server_properties
-        if self.server_properties is None:
-            self.server_properties = {}
-
-        self.task_holder = hp.TaskHolder(
-            self.final_future, name="WebServerRoutes::__init__[task_holder]"
-        )
-        self.graceful_final_future = hp.create_future(
-            name="WebServerRoutes::__init__[graceful_final_future]"
-        )
-
-    async def start(self, kls: type[Server] = Server):
-        assert self.server_properties is not None
-        WithProperties = type("S", (kls,), self.server_properties)  # type: tp.Any
-
-        class S(WithProperties):
-            async def setup_routes(s):
-                await super().setup_routes()
-                await self.setup_routes(s)
-
-        await self.task_holder.start()
-
-        self.server = S(
-            self.task_holder,
-            self.final_future,
-            self.graceful_final_future,
-        )
-
-        self.port = pytest.helpers.free_port()
-        self.serve_task = hp.get_event_loop().create_task(self.server.serve("127.0.0.1", self.port))
-        try:
-            await pytest.helpers.wait_for_port(self.port)
-        except AssertionError:
-            await self.serve_task
-            raise
-        return self
-
-    def stop(self):
-        self.graceful_final_future.cancel()
-
-    def start_request(self, method: str, route: str, body: dict | None = None) -> asyncio.Task:
-        async def request() -> aiohttp.ClientResponse:
-            async with aiohttp.ClientSession() as session:
-                return await getattr(session, method.lower())(
-                    f"http://127.0.0.1:{self.port}{route}", json=body
-                )
-
-        return hp.get_event_loop().create_task(request())
-
-    def run_stream(self, route: str, *items):
-        async def stream():
-            res = []
-            async with websockets.connect(f"ws://127.0.0.1:{self.port}{route}") as websocket:
-                for item in items:
-                    if not isinstance(item, str):
-                        await websocket.send(json.dumps(item))
-                    else:
-                        await websocket.send(item)
-
-                while True:
-                    try:
-                        res.append(await websocket.recv())
-                    except websockets.exceptions.ConnectionClosedOK:
-                        break
-            return res
-
-        return hp.get_event_loop().create_task(stream())
-
-    async def finish(self, exc_typ=None, exc=None, tb=None):
-        self.graceful_final_future.cancel()
-        hp.get_event_loop().call_later(20, self.serve_task.cancel)
-        await hp.wait_for_all_futures(self.serve_task)
-        try:
-            await self.server.finished()
-        except:
-            self.final_future.cancel()
-            await self.task_holder.finish()
