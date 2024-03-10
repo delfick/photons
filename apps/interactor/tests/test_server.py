6dc8e358149d32c7e40fa2a3fcc84be2e44ef02b
Stephen Moore <stephen@delfick.com>
2024-03-11 09:28:07 +1100

    interactor: remove needless mocks

--- apps/interactor/tests/test_server.py
+++ apps/interactor/tests/test_server.py
@@ -1,10 +1,7 @@
 # coding: spec
 
-from unittest import mock
-
 import pytest
 from interactor.commander.animations import Animations
-from interactor.commander.store import store
 from interactor.database import DB
 from interactor.zeroconf import Zeroconf
 from photons_app import helpers as hp
@@ -27,43 +24,8 @@ async def sender(devices, final_future):
         yield sender
 
 
-@pytest.fixture(scope="module")
-def V():
-    class V:
-        afr = mock.Mock(name="afr")
-        commander = mock.Mock(name="commander")
-
-        @hp.memoized_property
-        def lan_target(s):
-            m = mock.Mock(name="lan_target")
-
-            m.args_for_run = pytest.helpers.AsyncMock(name="args_for_run", return_value=s.afr)
-            m.close_args_for_run = pytest.helpers.AsyncMock(name="close_args_for_run")
-
-            return m
-
-        @hp.memoized_property
-        def FakeZeroconfRegisterer(s):
-            reg = mock.Mock(name="ZeroconfRegisterer")
-            reg.start = pytest.helpers.AsyncMock(name="start")
-            reg.finish = pytest.helpers.AsyncMock(name="start")
-            return reg
-
-    return V()
-
-
-@pytest.fixture(scope="module")
-async def server(V, server_wrapper, sender, final_future):
-    zeroconf_patch = mock.patch(
-        "interactor.zeroconf.ZeroconfRegisterer", lambda *a, **kw: V.FakeZeroconfRegisterer
-    )
-    with zeroconf_patch:
-        async with server_wrapper(store, sender, final_future) as server:
-            yield server
-
-
 describe "Server":
-    async it "starts things correctly", V, server:
+    async it "starts things correctly", server:
         tasks = server.ts
         server = server.server
         assert server.tasks is tasks
