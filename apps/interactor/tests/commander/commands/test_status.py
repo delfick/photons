b9c9fd60812bca4059dd43672fe1287db5b79dd5
Stephen Moore <stephen@delfick.com>
2024-03-11 14:40:46 +1100

    interactor: adding status command

---
+++ apps/interactor/tests/commander/commands/test_status.py
@@ -0,0 +1,16 @@
+# coding: spec
+
+from photons_app import mimic
+
+describe "Status":
+    describe "v1":
+
+        async it "has v1 routes", devices: mimic.DeviceCollection, server, responses:
+            await server.assertCommand(
+                "/v1/lifx/command", {"command": "status"}, json_output={"on": True}
+            )
+
+    describe "v2":
+
+        async it "GET /v2/status", devices: mimic.DeviceCollection, server, responses:
+            await server.assertMethod("GET", "/v2/status", json_output={"on": True})
